8d1c1752bd7003aefa367c08b1e0dcc00c1e3145
Faizan shaik <138827042+faizan736@users.noreply.github.com>
2024-01-29 21:08:07 +0530

    bestTimeToBuyAndSellStock.cpp

---
+++ Arrays/bestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,12 @@
+int bestTimeToBuyAndSellStock(vector<int>&prices) {
+    // Write your code here.
+    int mini = prices[0];
+    int maxprofit = 0;
+    int n = prices.size();
+    for(int i =1;i<n;i++){
+        int cost = prices[i]-mini;
+        maxprofit = max(maxprofit,cost);
+        mini = min(mini,prices[i]);
+    }
+    return maxprofit;
+}
