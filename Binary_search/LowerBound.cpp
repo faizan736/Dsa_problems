1240bdc7ca4a34c5f0659e6f1fd84c38abe6aad8
Faizan shaik <138827042+faizan736@users.noreply.github.com>
2024-02-05 21:25:37 +0530

    LowerBound.cpp

---
+++ Binary_search/LowerBound.cpp
@@ -0,0 +1,20 @@
+int lowerBound(vector<int> arr, int n, int x) {
+	// Write your code here
+	int s =0;
+	int e = n-1;
+	int mid = s+(e-s)/2;
+	int ans = n;
+	while(s<=e){
+		if(arr[mid] >= x){
+			ans = mid;
+			e = mid -1;
+		}
+		else{
+			s= mid +1 ;
+		}
+		mid = s+(e-s)/2;
+	}
+	return ans;
+
+	
+}
