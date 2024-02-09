99bf170c5798542c8f5c2fb5e4a22632f4872e77
Faizan shaik <138827042+faizan736@users.noreply.github.com>
2024-02-09 20:01:29 +0530

    Create findKRotation.cpp

---
+++ Binary_search/findKRotation.cpp
@@ -0,0 +1,18 @@
+int findKRotation(vector<int> &arr){
+    // Write your code here.    
+    	int s =0;
+	int e = arr.size()-1;
+	int mid = s+(e-s)/2;
+
+	while(s<e){
+    //why not arr[0] means we want to check in whole array
+		if(arr[mid] > arr[e]){
+			s= mid+1;
+		}
+		else{
+			e = mid;
+		}
+		mid = s+(e-s)/2;
+	}
+	return s;
+}
