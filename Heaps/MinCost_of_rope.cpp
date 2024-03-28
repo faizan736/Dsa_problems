a0a696049a5ef7397a1a87d3f6bf835ef8eebb28
Faizan shaik <138827042+faizan736@users.noreply.github.com>
2024-03-28 16:45:56 +0530

    Create MinCost_of_rope.cpp

---
+++ Heaps/MinCost_of_rope.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+        // Your code here
+        long long cost =0;
+        
+        //min heap
+        priority_queue<long long,vector<long long>,greater<long long>>pq;
+        for(int i =0;i<n;i++){
+            pq.push(arr[i]);
+        }
+        while(pq.size()>1){
+            long long a = pq.top();
+            pq.pop();
+            long long b = pq.top();
+            pq.pop();
+            
+            long long sum = a+b;
+            cost += sum;
+            pq.push(sum);
+        }
+        return cost;
+    }
+};
